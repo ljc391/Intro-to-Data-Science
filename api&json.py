47f3027c15cd9c39a25f8083b715a5c6250749ed
Elaine <ljc391@nyu.edu>
2016-08-18 22:18:49 -0400

    api & json

---
+++ api&json.py
@@ -0,0 +1,8 @@
+﻿import json
+import requests
+    if __name__==“__main__”:
+        url = “”
+        data = requests.get(url).text
+        data = json.loads(data)
+        print type(data)
+        print data
